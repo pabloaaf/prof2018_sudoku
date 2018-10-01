a7c151b295212b2dc719a137b637c8d39350d0a6
pabloaaf <palvaf02@estudiantes.unileon.es>
2018-10-01 18:29:14 +0200

    Sudoku Pablo Álvarez

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,74 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
-	public int verify (String candidateSolution)
-    {
-        return 0;
-    }
+public class SudokuVerifier {
+	public static int lengthSudoku = 9;
+	public static String[] numbersSudoku = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		// Leo el sudoku
+		verify(sc.next());
+		sc.close();
+	}
+
+	public static int verify(String candidateSolution) {
+		if (candidateSolution.contains("-") || candidateSolution.contains(".") || candidateSolution.contains(",")
+				|| candidateSolution.contains("0")) {
+			return -1;
+		}
+		if (candidateSolution.length() != 81) {
+			return -5;
+		}
+
+		String subcadena = "";
+		int x = 0;
+		int y = 0;
+		// error 2 ==> numero igual en subgrid
+		while (y < lengthSudoku) {
+			while (x < lengthSudoku) {
+				for (int i = x; i < x + (lengthSudoku / 3); i++) {
+					for (int j = y; j < y + (lengthSudoku / 3); j++) {
+						subcadena = subcadena.concat(candidateSolution.substring(i * 9 + j, i * 9 + j + 1));
+					}
+				}
+				for (int i = 0; i < lengthSudoku; i++) {
+					if (subcadena.indexOf(numbersSudoku[i]) != subcadena.lastIndexOf(numbersSudoku[i])) {
+						System.out.println(subcadena);
+						return -2;
+					}
+				}
+				subcadena = "";
+				x += 3;
+			}
+			x = 0;
+			y += 3;
+		}
+
+		// error 3 ==> numero igual en columna
+		for (int j = 0; j < lengthSudoku; j++) {
+			for (int i = 0; i < lengthSudoku; i++) {
+				subcadena = subcadena.concat(candidateSolution.substring(i * 9 + j, i * 9 + j + 1));
+			}
+			for (int i = 0; i < lengthSudoku; i++) {
+				if (subcadena.indexOf(numbersSudoku[i]) != subcadena.lastIndexOf(numbersSudoku[i]))
+					return -3;
+			}
+			subcadena = "";
+		}
+
+		// error 4 ==> numero igual en columna
+		for (int i = 0; i < lengthSudoku; i++) {
+			for (int j = 0; j < lengthSudoku; j++) {
+
+				subcadena = subcadena.concat(candidateSolution.substring(i * 9 + j, i * 9 + j + 1));
+			}
+			for (int j = 0; j < lengthSudoku; j++) {
+				if (subcadena.indexOf(numbersSudoku[j]) != subcadena.lastIndexOf(numbersSudoku[j]))
+					return -4;
+			}
+			subcadena = "";
+		}
+
+		return 0;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,79 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void testGeneralVerifyTrue() {
+		assertEquals(0, SudokuVerifier.verify("278539164"
+				+ "534216987"
+				+ "916874352"
+				+ "783452691"
+				+ "149687523"
+				+ "625193748"
+				+ "452761839"
+				+ "897325416"
+				+ "361948275"));
     }
+	
+	@Test
+	public void testGeneralVerifyFRule1() {
+		assertEquals(-1, SudokuVerifier.verify("278539164"
+				+ "534216987"
+				+ "916874352"
+				+ "783402691"
+				+ "149687523"
+				+ "625193748"
+				+ "452761839"
+				+ "897325416"
+				+ "361948275"));
+    }
+	@Test
+	public void testGeneralVerifyFRule2() {
+		assertEquals(-2, SudokuVerifier.verify("278539164"
+				+ "524216987"
+				+ "916874352"
+				+ "783452691"
+				+ "149687523"
+				+ "625193748"
+				+ "452761839"
+				+ "897325416"
+				+ "361948275"));
+    }
+	@Test
+	public void testGeneralVerifyFRule3() {
+		assertEquals(-3, SudokuVerifier.verify("278539164"
+				+ "534216587"
+				+ "916874392"
+				+ "783452691"
+				+ "149687523"
+				+ "625193748"
+				+ "452761839"
+				+ "897325416"
+				+ "361948275"));
+    }
+	@Test
+	public void testGeneralVerifyFRule4() {
+		assertEquals(-4, SudokuVerifier.verify("278539164"
+				+ "534216957"
+				+ "916874382"
+				+ "783452691"
+				+ "149687523"
+				+ "625193748"
+				+ "452761839"
+				+ "897325416"
+				+ "361948275"));
+    }
+	//ANSWER
+	//278539164
+	//534216987
+	//916874352
+	//783452691
+	//149687523
+	//625193748
+	//452761839
+	//897325416
+	//361948275
 }
